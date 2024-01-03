2d233af07ad2d63e30ed5639f8d6c0110f25b33d
Brandon Amiocsi <bamiosci@users.noreply.github.com>
2024-01-02 23:25:08 -0700

    Add process current page button to popup

--- chrome_extension/src/popup/popup.js
+++ chrome_extension/src/popup/popup.js
@@ -1,3 +1,12 @@
+import { registerDocument } from "./api";
+
+const getCurrentTab = async () => {
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  console.log(tab);
+  return tab;
+};
+
 const init = async () => {
   const extensionUrl = `chrome-extension://${chrome.runtime.id}`;
   const extensionHome = `${extensionUrl}/index.html`;
@@ -5,6 +14,17 @@ const init = async () => {
   const openSettingsButton = document.querySelector(".open-extension-page");
   openSettingsButton.addEventListener("click", async () => {
     window.open(extensionHome);
+  });
+
+  const processCurrentPageButton = document.querySelector(
+    ".process-current-page"
+  );
+  processCurrentPageButton.addEventListener("click", async () => {
+    const { title, url } = await getCurrentTab();
+    await registerDocument({
+      url,
+      title,
+    });
   });
 };
 
