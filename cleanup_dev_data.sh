685b8c541598f776597706cef8c026eaa0591f2d
Brandon Amiocsi <bamiosci@users.noreply.github.com>
2024-01-02 22:04:13 -0700

    Update cleanup script to use docker directly

--- cleanup_dev_data.sh
+++ cleanup_dev_data.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-export PGPASSWORD=`cat $HOME/.dev_secrets/knowledge_worker_postgres_passwd`
 
-psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "delete from kms.document_paths"
-psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "drop table kms.document_paths"
+# clean up database
+docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from kms.document_paths"
+docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table kms.document_paths"
+docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai.summaries"
+docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai.summaries"
+docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai.process_tasks"
+docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai.process_tasks"
+docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai_ops.process_task_metrics"
+docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai_ops.process_task_metrics"
 
-psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "delete from genai.summaries"
-psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "drop table genai.summaries"
+# clean up redis
+docker exec -it knowledge_agent_redis redis-cli flushall
 
-psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "delete from genai.process_tasks"
-psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "drop table genai.process_tasks"
-
-psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "delete from genai_ops.process_task_metrics"
-psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "drop table genai_ops.process_task_metrics"
-
-docker exec -it cabc2e300cd4 redis-cli flushall
-
+# stop ollama
 ps -aux | grep 'ollama serve' | grep -v grep | awk '{print $2}' | xargs -I @ sh -c 'sudo kill -9 @'
 
+# remove document CACHE
 rm -rf $HOME/kms
 
