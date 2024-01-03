4575c59725bfdc3a6899e0a19fa84699a120ccdf
Brandon Amiocsi <bamiosci@users.noreply.github.com>
2024-01-02 19:05:32 -0700

    Create basic dev data cleanup script

---
+++ cleanup_dev_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PGPASSWORD=`cat $HOME/.dev_secrets/knowledge_worker_postgres_passwd`
+
+psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "delete from kms.document_paths"
+psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "drop table kms.document_paths"
+
+psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "delete from genai.summaries"
+psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "drop table genai.summaries"
+
+psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "delete from genai.process_tasks"
+psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "drop table genai.process_tasks"
+
+psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "delete from genai_ops.process_task_metrics"
+psql -U knowledge_agent  -p 5432 -h 127.0.0.1 -c "drop table genai_ops.process_task_metrics"
+
+rm -rf $HOME/kms
+
+# ps -aux | grep 'ollama serve' | grep -v grep | awk '{print $2}' | xargs -I @ sh -c 'sudo kill -9 @'
