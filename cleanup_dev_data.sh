24f3cb1ced027a6ae60b90aeca2d8c829cdb37b4
Brandon Amiocsi <bamiosci@users.noreply.github.com>
2024-01-21 16:07:08 -0700

    Update database management scripts

--- cleanup_dev_data.sh
+++ cleanup_dev_data.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+read -p "Are you sure you wish to delete all development data? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # clean up database
+    docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from kms.document_paths"
+    docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table kms.document_paths"
+    docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai.summaries"
+    docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai.summaries"
+    docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai.process_tasks"
+    docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai.process_tasks"
+    docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai_ops.process_task_metrics"
+    docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai_ops.process_task_metrics"
 
-# clean up database
-docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from kms.document_paths"
-docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table kms.document_paths"
-docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai.summaries"
-docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai.summaries"
-docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai.process_tasks"
-docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai.process_tasks"
-docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "delete from genai_ops.process_task_metrics"
-docker exec -it knowledge_agent_postgres psql -U knowledge_agent -c "drop table genai_ops.process_task_metrics"
+    # clean up redis
+    docker exec -it knowledge_agent_redis redis-cli flushall
 
-# clean up redis
-docker exec -it knowledge_agent_redis redis-cli flushall
+    # stop ollama
+    ps -aux | grep 'ollama serve' | grep -v grep | awk '{print $2}' | xargs -I @ sh -c 'sudo kill -9 @'
 
-# stop ollama
-ps -aux | grep 'ollama serve' | grep -v grep | awk '{print $2}' | xargs -I @ sh -c 'sudo kill -9 @'
-
-# remove document CACHE
-rm -rf $HOME/kms
-
+    # remove document CACHE
+    rm -rf $HOME/kms
+fi
--- database/schema.sql
+++ database/schema.sql
@@ -1,60 +1,73 @@
-create schema genai;
+DO $do$
+BEGIN
+  IF EXISTS(
+    SELECT
+    FROM
+      pg_database
+    WHERE
+      datname = 'knowledge_agent') THEN
+  RAISE NOTICE 'Database already exists';
+ELSE
+  PERFORM
+    dblink_exec('dbname=' || current_database(), 'CREATE DATABASE knowledge_agent');
+END IF;
+END
+$do$;
 
-create schema genai_ops;
+CREATE SCHEMA genai;
 
-create schema kms;
+CREATE SCHEMA genai_ops;
 
-CREATE TABLE IF NOT EXISTS kms.document_paths (
-    hash char(64) primary key,
-    loader_spec jsonb NOT NULL,
-    metadata jsonb NOT NULL DEFAULT '{}' :: jsonb,
-    name text,
-    url text
+CREATE SCHEMA kms;
+
+CREATE TABLE IF NOT EXISTS kms.document_paths(
+  hash char(64) PRIMARY KEY,
+  loader_spec jsonb NOT NULL,
+  metadata jsonb NOT NULL DEFAULT '{}' ::jsonb,
+  name text,
+  url text
 );
 
-CREATE TABLE IF NOT EXISTS genai.summaries (
-    hash char(64) primary key,
-    summary text
+CREATE TABLE IF NOT EXISTS genai.summaries(
+  hash char(64) PRIMARY KEY,
+  summary text
 );
 
-CREATE TABLE IF NOT EXISTS genai.process_tasks (
-    hash char(64),
-    parent_id text not null,
-    -- nullable, set when started
-    task_id text,
-    -- nullable, only set if reprocess request is emitted
-    retry_task_id text,
-    task_name char(40),
-    status char(20),
-    status_reason text,
-    updated_at timestamp default now(),
-    primary key(hash, task_name)
+CREATE TABLE IF NOT EXISTS genai.process_tasks(
+  hash char(64),
+  parent_id text NOT NULL,
+  -- nullable, set when started
+  task_id text,
+  -- nullable, only set if reprocess request is emitted
+  retry_task_id text,
+  task_name char(40),
+  status char(20),
+  status_reason text,
+  updated_at timestamp DEFAULT now(),
+  PRIMARY KEY (hash, task_name)
 );
 
-CREATE FUNCTION sync_updated_at() RETURNS trigger AS $$
+CREATE OR REPLACE FUNCTION sync_updated_at()
+  RETURNS TRIGGER
+  AS $$
 BEGIN
   NEW.updated_at := NOW();
-
   RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
-CREATE TRIGGER
-  sync_updated_at
-BEFORE UPDATE ON
-  genai.process_tasks
-FOR EACH ROW EXECUTE PROCEDURE
-  sync_updated_at();
+CREATE OR REPLACE TRIGGER tr_sync_updated_at
+  BEFORE UPDATE ON genai.process_tasks
+  FOR EACH ROW
+  EXECUTE PROCEDURE sync_updated_at();
 
-
-CREATE TABLE IF NOT EXISTS genai_ops.process_task_metrics (
-    task_id text primary key,
-    -- null for root task
-    parent_id text,
-    -- metrics start
-    result char(30),
-    started_at timestamp,
-    duration real,
-    -- metrics end
-    CONSTRAINT fk_parent FOREIGN KEY(parent_id) REFERENCES genai_ops.process_task_metrics(task_id)
+CREATE TABLE IF NOT EXISTS genai_ops.process_task_metrics(
+  metric_id bigserial PRIMARY KEY,
+  task_id text,
+  -- metrics start
+  result char(30),
+  started_at timestamp,
+  duration real
 );
+
