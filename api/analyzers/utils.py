1288fb6e735bd348a56817ba6122ca4f2221e6e0
Brandon Amiocsi <bamiosci@users.noreply.github.com>
2024-02-25 13:35:14 -0700

    Update default language model

--- api/analyzers/utils.py
+++ api/analyzers/utils.py
@@ -4,7 +4,7 @@ from langchain.chat_models import ChatOllama
 from langchain_experimental.llms.ollama_functions import OllamaFunctions
 
 
-_DEFAULT_OLLAMA_MODEL = "llama2"
+_DEFAULT_OLLAMA_MODEL = "gemma:7b"
 
 
 def functions_llm(verbose: bool = False, **kwargs):
